b3188593e82ea5a03fe5405a32aae940da28d719
Filipe Sabella <me@filipesabella.com>
2022-09-12 10:50:54 -0300

    Better name

--- src/main/components/Main.tsx
+++ src/main/components/Main.tsx
@@ -6,17 +6,17 @@ import { api, Tag } from '../api';
 import { TagSelector } from './TagSelector';
 
 export const Main = () => {
-  const [text, setText] = useState(localStorage.getItem('lane_text') || '');
+  const [text, setText] = useState(localStorage.getItem('lane_previous_text') || '');
   const onChangeText = (text: string) => {
     setText(text);
-    localStorage.setItem('lane_text', text);
+    localStorage.setItem('lane_previous_text', text);
   };
 
   const [tags, setTags] = useState<Tag[]>(JSON.parse(
-    localStorage.getItem('lane_tags') || '[]'));
+    localStorage.getItem('lane_previous_tags') || '[]'));
   const onChangeTags = (tags: Tag[]) => {
     setTags(tags);
-    localStorage.setItem('lane_tags', JSON.stringify(tags));
+    localStorage.setItem('lane_previous_tags', JSON.stringify(tags));
   };
 
   const [saving, setSaving] = useState(false);
