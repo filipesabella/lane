791733b5baebd189e7b4203c942413fa784b91fd
Filipe Sabella <me@filipesabella.com>
2022-09-11 23:52:56 -0300

    Add encryption password setting

--- src/main/components/Settings.tsx
+++ src/main/components/Settings.tsx
@@ -6,15 +6,20 @@ import { storage } from '../storage';
 export const Settings = () => {
   const [supabaseUrl, setSupabaseUrl] = useState('');
   const [supabaseKey, setSupabaseKey] = useState('');
+  const [encryptionPassword, setEncryptionPassword] = useState('');
 
   useEffect(() => {
-    const { supabaseUrl, supabaseKey } = storage.loadSettings();
+    const {
+      supabaseUrl,
+      supabaseKey,
+      encryptionPassword, } = storage.loadSettings();
     setSupabaseUrl(supabaseUrl);
     setSupabaseKey(supabaseKey);
+    setEncryptionPassword(encryptionPassword);
   }, []);
 
   const save = () => {
-    storage.storeSettings({ supabaseUrl, supabaseKey });
+    storage.storeSettings({ supabaseUrl, supabaseKey, encryptionPassword, });
   };
 
   return <div className="settings">
@@ -22,14 +27,18 @@ export const Settings = () => {
     <div className="fields">
       <label>Supabase URL</label>
       <input
-        type="text"
         value={supabaseUrl}
-        onChange={e => setSupabaseUrl(e.currentTarget.value)}></input>
+        onChange={e => setSupabaseUrl(e.currentTarget.value)} />
       <label>Supabase Key</label>
       <input
-        type="text"
         value={supabaseKey}
-        onChange={e => setSupabaseKey(e.currentTarget.value)}></input>
+        onChange={e => setSupabaseKey(e.currentTarget.value)} />
+      <label>Encryption Password</label>
+      <input
+        value={encryptionPassword}
+        type="password"
+        placeholder="If you lose or change this all the data will be lost"
+        onChange={e => setEncryptionPassword(e.currentTarget.value)} />
     </div>
     <button onClick={_ => save()}>save</button>
   </div>;
